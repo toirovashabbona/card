da604644fe6594ceff5d753e4a059d2eb986d91b
toirovashabbona <toirovashabbona167@gmail.com>
2024-06-03 08:14:31 +0500

    Add files via upload

---
+++ 29dars/script.js
@@ -0,0 +1,33 @@
+// var a = 100;
+// var b = 0;
+// while(a >= b){
+//     console.log(b);
+//      b++
+// }
+
+// var gul = ("barglari, ildizi, gullari")
+// console.log(gul)
+
+
+// var person = {
+//     name: "Ziyoda",
+//     age: 20,
+//     car: true,
+//     height: 1.60,
+//     family: true
+// }
+// console.log("Ziyoda 20 yosh ")
+
+// var r = "salom"
+// function carD(k){
+//     console.log(k.replace(k.slice(5, 14), "**** ****"));
+// }
+// carD("8600 2203 0803 0303")
+
+// var k = prompt("sonni kirgizing")
+// function carD(k){
+//     console.log(k.replace(k.slice(5, 14),  "**** ****"));
+// }
+// carD("8600 2203 0803 0303")
+
+
